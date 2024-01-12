ebb5f637c2ce64c396485b6e467b5217666682b1
Tanawut Wongboot <tanawut.w@outlook.com>
2024-01-12 12:09:39 +0700

    feat: connect server with mongodb

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,12 +1,36 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const products = await collection.find().toArray();
+
+    return res.json({
+      data: products,
+    });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+});
 
 productRouter.get("/:id", (req, res) => {});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("products");
+
+  const productData = { ...req.body };
+  const products = await collection.insertOne(productData);
+
+  return res.json({
+    message: `Product record (${products.insertedId}) has been created successfully`,
+  });
+});
 
 productRouter.put("/:id", (req, res) => {});
 
